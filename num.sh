316994994d527a07c3e4290a971490bcf07a3967
yurkapotam <111305715+yurkapotam@users.noreply.github.com>
2024-11-19 13:24:34 +0300

    Update num.sh

--- num.sh
+++ num.sh
@@ -2,7 +2,7 @@ echo "AMOUNT is $AMOUNT"
 echo "ACTOR is $ACTOR"
 git config --global user.name "${ACTOR}"
 git config --global user.email "${ACTOR}@users.noreply.github.com"
-pwsh ./num.ps1 $AMOUNT
+pwsh ./num.ps1 -amount $AMOUNT
 git add .
 git commit -m 'num'
 git push origin HEAD:main
