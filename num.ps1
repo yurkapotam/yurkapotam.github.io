611dc39b5bf3b6f4861ed0eb634c8d087eb40e34
yurkapotam <111305715+yurkapotam@users.noreply.github.com>
2024-11-19 13:42:02 +0300

    Update num.ps1

--- num.ps1
+++ num.ps1
@@ -6,7 +6,7 @@ cd num
 $cur = ""
 1..$amount | % {
     $cur += $_
-    $cur += "\n"
+    $cur += "`n"
 }
 $cur | Out-File -FilePath "${amount}.txt"
 $nums = Get-ChildItem ./*.txt -Name
@@ -14,6 +14,6 @@ cd ..
 $cur = ""
 foreach ($i in $nums) {
     $cur += $i
-    $cur += "\n"
+    $cur += "`n"
 }
 $cur | Out-File -FilePath "nums.txt"
