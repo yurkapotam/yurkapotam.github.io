95dce9de9aa25f81c1ef12d610e7c86d1ef00347
yurkapotam <111305715+yurkapotam@users.noreply.github.com>
2024-11-29 20:39:16 +0300

    Create mc-to-lac.ps1

---
+++ mc-to-lac.ps1
@@ -0,0 +1,54 @@
+param (
+    [string]$_savepath
+    [savetype]$savetype = [savetype]::first
+    [string]$mapname
+)
+
+enum savetype
+{
+    first
+    second
+    third
+}
+
+$savepath = if ($_savepath.EndsWith("/")) {$_savepath} else {$_savepath + "/"}
+
+$cur =  "Map Name:$mapname`n"
+$cur += "Map Type:1`n"
+$cur += "Holo Sign:0.0, 0.0, 0.0*`n"
+$cur += "Camera Pos:0.00, 0.00, 0.00:0.00, 0.00, 0.00`n"
+$cur += "Max Vehicle Count: 16`n"
+$cur += "Fuel Consume Rate: 2`n"
+$cur += "Delete Idle Vehicle: -1`n"
+$cur += "Health Regeneration: true`n"
+$cur += "Hide Names: false`n"
+$cur += "Allow Respawn: true`n"
+$cur += "Voice-Chat: enabled`n"
+$cur += "Vote For Role: disabled`n"
+$cur += "Role-play: disabled`n"
+$cur += "Approve On Register: false`n"
+$cur += "Roles List:<color=yellow>[TAXI]</color>,<color=#C7C>[WORKER]</color>,[BUILDER],<color=green>[DOCTOR]</color>,<color=orange>[MECHANIC]</color>,<color=blue>[POLICE]</color>,<color=red>[ADMIN]</color>,`n"
+
+switch ($savetype)
+{
+    [savetype]::first
+    {
+        $level = [System.IO.File]::ReadAllBytes($savepath)
+        for ($x = 0; $x < 256; $x++)
+        {
+            for ($y = 0; $y < 64; $y++)
+            {
+                for ($z = 0; $z < 256; $z++)
+                {
+                    $id = $level[65536 * $y + 256 * $z + $x]
+                    if ($id != 0) {$cur += "Block_Scalable_Editor:$($x * 2).00,$($y * 2).00,$($z * 2).00:0.00,0.00,0.00:2.00,2.00,2.00:color{1.00,1.00,1.00} material{${id},0.25}`n"}
+                }
+            }
+        }
+
+        $cur += "Downloadable_Content_Material|https://yurkapotam.github.io/mc_first/1.jpg`n"
+        $cur += "Downloadable_Content_Material|https://yurkapotam.github.io/mc_first/2.jpg`n"
+
+        $cur | Out-File -FilePath $_savepath
+    }
+}
