a4ba32b5e645e19ed267641281c265352e9ae4c3
yurkapotam <111305715+yurkapotam@users.noreply.github.com>
2024-11-30 13:20:00 +0300

    Update mc-to-lac.sh

--- mc-to-lac.sh
+++ mc-to-lac.sh
@@ -3,7 +3,7 @@ echo "OUTPATH is $OUTPATH"
 git config --global user.name "$ACTOR"
 git config --global user.email "${ACTOR}@users.noreply.github.com"
 mcs -out mc-to-lac.exe mc-to-lac.cs
-mono mc-to-lac.exe
+mono mc-to-lac.exe $SAVEPATH 0 $OUTPATH mctolac
 git add .
 git commit -m 'mc-to-lac'
 git push origin HEAD:main
